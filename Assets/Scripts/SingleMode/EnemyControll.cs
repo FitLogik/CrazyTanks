2df1166594321884df68983f5c2591c84f6de456
FitLogik <rogozin.vanechka@yandex.ru>
2024-05-20 23:10:06 +0300

    Deleted unnecessary files

--- Assets/Scripts/SingleMode/EnemyControll.cs
+++ Assets/Scripts/SingleMode/EnemyControll.cs
@@ -9,26 +9,22 @@ public class EnemyControll : MonoBehaviour
     public float horizontalSpeed = 0.5f;
     public float health = 1.0f;
    
-
     private float minHeight = 0f;
     private float maxHeight = 2f;
     private float targetXPosition = 10.1f; // Задаем целевую позицию по оси X
 
     private Score sp;
-    private BulletEnemy cs;
 
     public bool ComboScore = false;
 
 
     private void Start()
     {
-        cs = FindObjectOfType<BulletEnemy>();
         sp = FindObjectOfType<Score>();
     }
     void Update()
     {
         transform.Translate(Vector3.right * -horizontalSpeed * Time.deltaTime);
-
     }
 
     public void TakeDamage()
